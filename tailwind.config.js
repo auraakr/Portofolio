6c7a0d1efa58538236d71dfc003cf20886bbb125
Aura Putri <aura232kireina@gmail.com>
2023-11-14 21:23:27 +0700

    add all files

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./build/*.html'],
+  theme: {
+    extend: {
+      // color: {
+      //   abu/body: #646B71,
+      //   abu/section: #B0B7BD,
+      //   black/title: #020406,
+      //   white: #fff;
+      // }
+      fontFamily: {
+        dmsans: ['DM Sans', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
